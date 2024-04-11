16b367c6870332dac0f7c2ee8cd7a01d8d55d9c1
phoniexorama <69302560+phoniexorama@users.noreply.github.com>
2024-04-11 10:23:17 +0200

    Update stepTestSeriesGenerator.groovy

--- resources/vars/stepTestSeriesGenerator.groovy
+++ resources/vars/stepTestSeriesGenerator.groovy
@@ -1,1 +1,14 @@
-h
+def runMyPython() {
+    // Fetch Python script content from library resource
+    def pythonContent = libraryResource('resources/script/testseriesgenerator.py')
+
+    // Write Python script to a file in the Jenkins workspace
+    writeFile(file: 'testseriesgenerator.py', text: pythonContent)
+
+    // Use correct file path separator for Windows
+    def pythonScriptPath = "${env.WORKSPACE}\\testseriesgenerator.py"
+
+    // Execute Python script using Python interpreter (python or python3) depending on your setup
+    // Make sure 'python' is in the system PATH or provide full path to the Python interpreter
+    bat "python ${pythonScriptPath}"
+}
