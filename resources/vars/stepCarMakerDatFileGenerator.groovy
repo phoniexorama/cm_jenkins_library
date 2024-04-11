5324fb77a0c0751c38c90983b6fc4028dc4ac262
phoniexorama <69302560+phoniexorama@users.noreply.github.com>
2024-04-11 10:18:25 +0200

    Create stepCarMakerDatFileGenerator.groovy

---
+++ resources/vars/stepCarMakerDatFileGenerator.groovy
@@ -0,0 +1,14 @@
+def runMyPython() {
+    // Fetch Python script content from library resource
+    def pythonContent = libraryResource('resources/script/carmakerdatfilegenerator.py')
+
+    // Write Python script to a file in the Jenkins workspace
+    writeFile(file: 'carmakerdatfilegenerator.py', text: pythonContent)
+
+    // Use correct file path separator for Windows
+    def pythonScriptPath = "${env.WORKSPACE}\\carmakerdatfilegenerator.py"
+
+    // Execute Python script using Python interpreter (python or python3) depending on your setup
+    // Make sure 'python' is in the system PATH or provide full path to the Python interpreter
+    bat "python ${pythonScriptPath}"
+}
