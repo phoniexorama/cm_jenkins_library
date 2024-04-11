9522da3b18901dd482761225d7840536eff6a764
phoniexorama <69302560+phoniexorama@users.noreply.github.com>
2024-04-11 12:06:44 +0200

    Update stepAutoExcelFileGenerator.groovy

--- vars/stepAutoExcelFileGenerator.groovy
+++ vars/stepAutoExcelFileGenerator.groovy
@@ -1,6 +1,6 @@
 def call() {
     // Fetch Python script content from library resource
-    def pythonContent = libraryResource('resources/scripts/autoexcelfilegenerator.py')
+    def pythonContent = libraryResource('scripts/autoexcelfilegenerator.py')
 
     // Write Python script to a file in the Jenkins workspace
     writeFile(file: 'autoexcelfilegenerator.py', text: pythonContent)
