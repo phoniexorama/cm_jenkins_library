5c8e0288c6df352e939b326d41d738739dfade2c
phoniexorama <69302560+phoniexorama@users.noreply.github.com>
2024-04-12 12:55:52 +0200

    Update stepCopyFolder.groovy

--- vars/stepCopyFolder.groovy
+++ vars/stepCopyFolder.groovy
@@ -1,5 +1,4 @@
-// Define a Groovy function to copy files using xcopy (Windows)
-def copyFiles(List<String> sourcePaths, String destinationPath) {
+def call(List<String> sourcePaths, String destinationPath) {
     // Iterate over each source path and copy to the destination path
     sourcePaths.each { sourcePath ->
         // Use 'bat' step to execute batch command for copying
