b7c71fcd151a94d53eb944127897e7e54eb0f74b
phoniexorama <69302560+phoniexorama@users.noreply.github.com>
2024-04-15 09:57:02 +0200

    Update stepCopyFolder.groovy

--- vars/stepCopyFolder.groovy
+++ vars/stepCopyFolder.groovy
@@ -1,8 +1,9 @@
+// Define the copyFolder function
 def call(List<String> sourcePaths, String destinationPath) {
     // Iterate over each source path and copy to the destination path
     sourcePaths.each { sourcePath ->
         // Use 'bat' step to execute batch command for copying
-        bat "xcopy \"${sourcePath}\" \"${destinationPath}\" /E /Y"
+        bat "xcopy \"${sourcePath}\" \"${destinationPath}\" /E /I /Y"
         println "Successfully copied '${sourcePath}' to '${destinationPath}'"
     }
 }
