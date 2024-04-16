15dc6aec7bd77cc0038799a3293af734f2fc5e0e
phoniexorama <69302560+phoniexorama@users.noreply.github.com>
2024-04-16 16:33:29 +0200

    Update carmakerTestseries.bat

--- resources/bat/carmakerTestseries.bat
+++ resources/bat/carmakerTestseries.bat
@@ -19,7 +19,7 @@ REM 2) APO broker daemon (automatically started, just if needed to close it)
 SET APOBD=%CMDIR%\bin\apobrokerd.exe
 
 REM 3) The CarMaker project, this sets the Tcl variable HIL(ProjDir)
-SET CMPRJDIR=C:\CM_Test\Frg-Bedatung_Cayenne_E4_CM12
+SET CMPRJDIR=C:\JenkinsAgent\workspace\CarMaker\Frg-Bedatung_Cayenne_E4_CM12
 
 REM 4) The (project related) path of the CarMaker application
 REM    to calculate the simulation
@@ -29,11 +29,11 @@ SET CMAPP=CarMaker.win64.exe
 
 REM 5) Some TestSerie and TestRun example to be simulated
 SET TSFNAME=PO546_E4_Cayenne_SUV_TOP_VFF.ts
-SET TSFPATH=C:/CM_Test/Frg-Bedatung_Cayenne_E4_CM12/Data/TestRun/%TSFNAME%
+SET TSFPATH=C:\JenkinsAgent\workspace\CarMaker\Frg-Bedatung_Cayenne_E4_CM12/Data/TestRun/%TSFNAME%
 
 REM 6) The Tcl script to remote control the CarMaker GUI
 SET TCLFNAME=CMGUI_RemCtrl.tcl
-SET TCLFPATH=C:/CM_Test/Frg-Bedatung_Cayenne_E4_CM12/Data/Script/Examples/%TCLFNAME%
+SET TCLFPATH=C:\JenkinsAgent\workspace\CarMaker\Frg-Bedatung_Cayenne_E4_CM12/Data/Script/Examples/%TCLFNAME%
 
 REM 7) Control flags
 SET SIM_TS=1
