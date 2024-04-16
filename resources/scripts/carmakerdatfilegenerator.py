510392a9d9fc01ddb77549dc93d8d646de52dcc6
phoniexorama <69302560+phoniexorama@users.noreply.github.com>
2024-04-16 10:26:02 +0200

    Update carmakerdatfilegenerator.py

--- resources/scripts/carmakerdatfilegenerator.py
+++ resources/scripts/carmakerdatfilegenerator.py
@@ -4,10 +4,11 @@ import subprocess
 from datetime import datetime
 import time
 import sys
-# Add the directory containing the shared scripts to Python's module search path
-sys.path.append(os.path.join(os.path.dirname(__file__), 'resources', 'scripts'))
 
-from scripts.process_utils import wait_until_process_terminated
+# Add the directory containing the shared scripts to Python's module search path
+sys.path.append(os.path.join(os.path.dirname(__file__), '..', 'resources', 'scripts'))
+
+from process_utils import wait_until_process_terminated
 
 def replace_vehicle_value(template_folder_path, new_vehicle_values):
     #time.sleep(6)  # Wait for 6 seconds
