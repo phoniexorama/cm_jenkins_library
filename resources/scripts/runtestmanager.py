e86849f67211e58ffb4c690b76ea2abff41d67fc
phoniexorama <69302560+phoniexorama@users.noreply.github.com>
2024-04-16 14:59:04 +0200

    Update runtestmanager.py

--- resources/scripts/runtestmanager.py
+++ resources/scripts/runtestmanager.py
@@ -78,10 +78,12 @@ def run_batch_script(script_path):
 
 
 if __name__ == "__main__":
-    batch_script_path = "C:/CM_Test/Frg-Bedatung_Cayenne_E4_CM12/carmakerTestseries.bat"
-    test_series_folder_path = "C:/CM_Test/Frg-Bedatung_Cayenne_E4_CM12/Data/TestRun"
-    # batch_script_path = os.environ.get('BATCH_SCRIPT_PATH')
-    # test_series_folder_path = os.environ.get('TEST_SERIES_FOLDER_PATH')
+    #batch_script_path = "C:/CM_Test/Frg-Bedatung_Cayenne_E4_CM12/carmakerTestseries.bat"
+    #test_series_folder_path = "C:/CM_Test/Frg-Bedatung_Cayenne_E4_CM12/Data/TestRun"
+    batch_script_path = os.environ.get('BATCH_SCRIPT_PATH')
+    test_series_folder_path = os.environ.get('TEST_SERIES_FOLDER_PATH')
+    print(batch_script_path)
+    print(test_series_folder_path)
     format_file_config_path = os.environ.get('FORMAT_FILE_CONFIG_PATH')
 
     erg_format = 'DStore.Format = erg\n'
