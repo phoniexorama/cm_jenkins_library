014ba880ad0722f190e95eacdadc9735e9ed7692
phoniexorama <69302560+phoniexorama@users.noreply.github.com>
2024-04-16 10:32:39 +0200

    Update carmakerdatfilegenerator.py

--- resources/scripts/carmakerdatfilegenerator.py
+++ resources/scripts/carmakerdatfilegenerator.py
@@ -3,6 +3,21 @@ import shutil
 import subprocess
 from datetime import datetime
 import time
+import psutil
+
+def is_process_running(process_name):
+    """Check if a process with the given name is currently running."""
+    for proc in psutil.process_iter(['pid', 'name']):
+        if proc.info['name'] == process_name:
+            return True
+    return False
+
+def wait_until_process_terminated(process_name):
+    """Wait until a process with the given name is terminated."""
+    while is_process_running(process_name):
+        time.sleep(5)  # Check every 5 seconds
+
+    print(f"{process_name} is no longer running.")
 
 def replace_vehicle_value(template_folder_path, new_vehicle_values):
     #time.sleep(6)  # Wait for 6 seconds
